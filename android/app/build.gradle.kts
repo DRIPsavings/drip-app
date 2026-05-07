9eb2e5a7232872aba336ad2b993f6181f7f2ddfb
DRIPsavings <territorysales@hotmail.com>
2026-05-07 22:42:04 +0000

    Update ndkVersion to 28.2.13676358 for plugin compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.drip"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "26.1.10909125"
+    ndkVersion = "28.2.13676358"   // ← Updated for google_maps_flutter & speech_to_text
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,10 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID.
         applicationId = "com.example.drip"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +30,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
