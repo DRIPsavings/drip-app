c76bc3be6eb43341721cd797b0b0f80ce90d463e
DRIPsavings <territorysales@hotmail.com>
2026-05-08 20:00:18 +0000

    Fix NDK version to 27.1.12297007 for plugins

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.drip"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.1.12297007"   // Fixed for google_maps_flutter + speech_to_text
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.drip"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
